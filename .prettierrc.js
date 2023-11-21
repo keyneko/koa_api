12ce82d670bcc5eefb74c4f806651f560cdccdaa
周翔 <keyneko@163.com>
2023-11-21 18:10:04 +0800

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+}
