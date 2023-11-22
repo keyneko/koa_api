d5ad38e7958063e08575e2788052458c92677f1e
周翔 <keyneko@163.com>
2023-11-22 17:47:56 +0800

    完成用户注册、登录接口

--- app/app.js
+++ app/app.js
@@ -1,24 +1,29 @@
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
-const blogRoutes = require('./routes/blogRoutes');
+const Koa = require('koa')
+const { koaBody } = require('koa-body')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
+const blogRoutes = require('./routes/blogRoutes')
 
-const app = new Koa();
-const PORT = process.env.PORT || 3000;
-const MONGODB_URI = 'mongodb://localhost:27017/your-database-name';
+const app = new Koa()
+const PORT = process.env.PORT || 3000
+const MONGODB_URI = 'mongodb://localhost:27017/test'
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // Middleware
-app.use(bodyParser());
+app.use(
+  koaBody({
+    multipart: true,
+    formidable: { maxFieldsSize: 5 * 1024 * 1024 },
+  }),
+)
 
 // Routes
-app.use(authRoutes.routes());
-app.use(blogRoutes.routes());
+app.use(authRoutes.routes())
+app.use(blogRoutes.routes())
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,52 +1,156 @@
-const Router = require('koa-router');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const Router = require('koa-router')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const svgCaptcha = require('svg-captcha')
 
-const authRouter = new Router();
+const authRouter = new Router()
+const secretKey = 'your-secret-key' // Replace with a secure key for signing JWT
 
-const secretKey = 'your-secret-key'; // Replace with a secure key for signing JWT
+// 存储验证码文本和对应的验证码ID的对象
+const captchaStore = {}
+
+// 定时清理过期验证码
+setInterval(() => {
+  const now = Date.now()
+  for (const [captchaId, { text, timestamp }] of Object.entries(captchaStore)) {
+    if (now - timestamp > 5 * 60 * 1000) {
+      console.log(`captcha expired: ${text}`)
+      // 超过5分钟的验证码将被清理
+      delete captchaStore[captchaId]
+    }
+  }
+}, 60 * 1000) // 每分钟检查一次
+
+authRouter.get('/captcha', async (ctx) => {
+  const captcha = svgCaptcha.create()
+  const captchaId = Date.now().toString()
+
+  console.log(`captcha generated: ${captcha.text} ${captchaId}`)
+
+  captchaStore[captchaId] = {
+    text: captcha.text.toLowerCase(), // 存储验证码文本，转为小写以方便后续验证
+    timestamp: Date.now(),
+  }
+
+  ctx.type = 'svg'
+  ctx.body = {
+    captchaId,
+    captcha: captcha.data,
+  }
+})
 
 authRouter.post('/register', async (ctx) => {
-  // ... (unchanged)
+  const { username, password, captcha, captchaId } = ctx.request.body
+
+  // 验证验证码是否匹配
+  const storedCaptcha = captchaStore[captchaId]
+  if (!storedCaptcha || storedCaptcha.text !== captcha.toLowerCase()) {
+    ctx.status = 400
+    ctx.body = {
+      code: 400,
+      message: 'Invalid captcha',
+    }
+    return
+  }
+  // 清除验证码
+  delete captchaStore[captchaId]
 
   try {
-    // ... (unchanged)
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username })
+    if (existingUser) {
+      ctx.status = 409 // Conflict
+      ctx.body = {
+        code: 409,
+        message: 'Username already exists',
+      }
+      return
+    }
 
-    const token = generateToken(newUser); // Generate JWT
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-    ctx.status = 201; // Created
-    ctx.body = { message: 'Registration successful', token };
+    // Create a new user
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    })
+
+    // Save the user to the database
+    await newUser.save()
+
+    const token = generateToken(newUser) // Generate JWT
+
+    ctx.status = 201 // Created
+    ctx.body = {
+      code: 200,
+      message: 'Registration successful',
+      token,
+    }
   } catch (error) {
-    ctx.status = 500; // Internal Server Error
-    ctx.body = 'An error occurred during registration';
+    ctx.status = 500 // Internal Server Error
+    ctx.body = {
+      code: 500,
+      message: 'An error occurred during registration',
+    }
   }
-});
+})
 
 authRouter.post('/login', async (ctx) => {
-  // ... (unchanged)
+  const { username, password, captcha, captchaId } = ctx.request.body
+
+  // 验证验证码是否匹配
+  const storedCaptcha = captchaStore[captchaId]
+  if (!storedCaptcha || storedCaptcha.text !== captcha.toLowerCase()) {
+    ctx.status = 400
+    ctx.body = {
+      code: 400,
+      message: 'Invalid captcha',
+    }
+    return
+  }
+  // 清除验证码
+  delete captchaStore[captchaId]
 
   try {
-    // ... (unchanged)
+    // Find the user in the database
+    const user = await User.findOne({ username })
 
-    const token = generateToken(user); // Generate JWT
+    // Check if the user exists and verify the password
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = generateToken(user) // Generate JWT
 
-    ctx.status = 200; // OK
-    ctx.body = { message: 'Login successful', token };
+      ctx.status = 200 // OK
+      ctx.body = {
+        code: 200,
+        message: 'Login successful',
+        token,
+      }
+    } else {
+      ctx.status = 401 // Unauthorized
+      ctx.body = {
+        code: 401,
+        message: 'Invalid credentials',
+      }
+    }
   } catch (error) {
-    ctx.status = 500; // Internal Server Error
-    ctx.body = 'An error occurred during login';
+    ctx.status = 500 // Internal Server Error
+    ctx.body = {
+      code: 500,
+      message: 'An error occurred during login',
+    }
   }
-});
+})
 
 function generateToken(user) {
   const payload = {
     userId: user._id,
     username: user.username,
-  };
+  }
 
   // Sign the JWT token
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' }); // Adjust the expiration time as needed
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' }) // Adjust the expiration time as needed
 }
 
-module.exports = authRouter;
+module.exports = authRouter
