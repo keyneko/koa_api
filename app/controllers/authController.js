ff35af42dd705674c039b417f2194bf15495fc66
周翔 <keyneko@163.com>
2023-11-23 23:49:16 +0800

    逻辑移至controllers

---
+++ app/controllers/authController.js
@@ -0,0 +1,158 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const svgCaptcha = require('svg-captcha')
+const User = require('../models/user')
+
+const secretKey = 'your-secret-key' // Replace with a secure key for signing JWT
+
+// 存储验证码文本和对应的验证码ID的对象
+const captchaStore = {}
+
+// 定时清理过期验证码
+setInterval(() => {
+  const now = Date.now()
+  for (const [captchaId, { text, timestamp }] of Object.entries(captchaStore)) {
+    if (now - timestamp > 5 * 60 * 1000) {
+      console.log(`captcha expired: ${text}`)
+      // 超过5分钟的验证码将被清理
+      delete captchaStore[captchaId]
+    }
+  }
+}, 60 * 1000) // 每分钟检查一次
+
+function generateToken(user) {
+  const payload = {
+    userId: user._id,
+    username: user.username,
+  }
+
+  // Sign the JWT token
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' }) // Adjust the expiration time as needed
+}
+
+function captcha(ctx) {
+  const captcha = svgCaptcha.create()
+  const captchaId = Date.now().toString()
+
+  console.log(`captcha generated: ${captcha.text} ${captchaId}`)
+
+  captchaStore[captchaId] = {
+    text: captcha.text.toLowerCase(), // 存储验证码文本，转为小写以方便后续验证
+    timestamp: Date.now(),
+  }
+
+  ctx.type = 'svg'
+  ctx.body = {
+    captchaId,
+    captcha: captcha.data,
+  }
+}
+
+async function register(ctx) {
+  const { username, password, captcha, captchaId } = ctx.request.body
+
+  // 验证验证码是否匹配
+  const storedCaptcha = captchaStore[captchaId]
+  if (!storedCaptcha || storedCaptcha.text !== captcha.toLowerCase()) {
+    ctx.status = 400
+    ctx.body = {
+      code: 400,
+      message: 'Invalid captcha',
+    }
+    return
+  }
+  // 清除验证码
+  delete captchaStore[captchaId]
+
+  try {
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username })
+    if (existingUser) {
+      ctx.status = 409 // Conflict
+      ctx.body = {
+        code: 409,
+        message: 'Username already exists',
+      }
+      return
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    })
+
+    // Save the user to the database
+    await newUser.save()
+
+    const token = generateToken(newUser) // Generate JWT
+
+    ctx.status = 201 // Created
+    ctx.body = {
+      code: 200,
+      message: 'Registration successful',
+      token,
+    }
+  } catch (error) {
+    ctx.status = 500 // Internal Server Error
+    ctx.body = {
+      code: 500,
+      message: 'An error occurred during registration',
+    }
+  }
+}
+
+async function login(ctx) {
+  const { username, password, captcha, captchaId } = ctx.request.body
+
+  // 验证验证码是否匹配
+  const storedCaptcha = captchaStore[captchaId]
+  if (!storedCaptcha || storedCaptcha.text !== captcha.toLowerCase()) {
+    ctx.status = 400
+    ctx.body = {
+      code: 400,
+      message: 'Invalid captcha',
+    }
+    return
+  }
+  // 清除验证码
+  delete captchaStore[captchaId]
+
+  try {
+    // Find the user in the database
+    const user = await User.findOne({ username })
+
+    // Check if the user exists and verify the password
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = generateToken(user) // Generate JWT
+
+      ctx.status = 200 // OK
+      ctx.body = {
+        code: 200,
+        message: 'Login successful',
+        token,
+      }
+    } else {
+      ctx.status = 401 // Unauthorized
+      ctx.body = {
+        code: 401,
+        message: 'Invalid credentials',
+      }
+    }
+  } catch (error) {
+    ctx.status = 500 // Internal Server Error
+    ctx.body = {
+      code: 500,
+      message: 'An error occurred during login',
+    }
+  }
+}
+
+module.exports = {
+  captcha,
+  register,
+  login,
+}
