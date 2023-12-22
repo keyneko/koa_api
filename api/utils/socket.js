7dbefb2650d6c9e3f67f6eeb9d14e770390c5d8f
周翔 <keyneko@163.com>
2023-12-22 18:48:28 +0800

    fix: 维护Socket客户端列表

--- api/utils/socket.js
+++ api/utils/socket.js
@@ -2,6 +2,8 @@
 const socketIO = require('socket.io')
 const { verifyToken } = require('../controllers/authController')
 
+const connectedClients = new Map()
+
 function initializeSocket(server) {
   const io = socketIO(server)
 
@@ -17,15 +19,36 @@ function initializeSocket(server) {
 
     console.log('Client connected:', socket.id)
 
+    connectedClients.set(socket.id, {
+      userId: decoded.userId,
+      socket: socket,
+    })
+
     // Add the logic after connection here
     socket.on('joinRoom', (data) => {
       console.log('Received:', data)
 
       socket.emit('message', 'Hello, client!')
     })
+
+    // Triggered when client disconnects
+    socket.on('disconnect', () => {
+      console.log('Client disconnected:', socket.id)
+
+      // Removed from Map when client disconnects
+      connectedClients.delete(socket.id)
+    })
   })
 
   return io
 }
+
+// // 通过 connectedClients Map 对象可以向特定客户端发送消息
+// function sendMessageToClient(clientId, event, data) {
+//   const client = connectedClients.get(clientId);
+//   if (client && client.socket) {
+//     client.socket.emit(event, data);
+//   }
+// }
 
 module.exports = { initializeSocket }
